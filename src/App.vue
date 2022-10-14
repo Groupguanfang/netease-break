9f3725b4807b169d769d8b1d17cd02f13cd52033
GCZ-Zero <1203970284@qq.com>
2022-10-14 05:47:49 +0000

    all

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { getLink } from "./api"
+
+export default {
+  name: "app",
+  data() {
+    return {
+      url: "",
+      id: "",
+      buttonText: "请填写音乐id",
+      disabled: true
+    }
+  },
+  methods: {
+    async action() {
+      this.buttonText = "加载中"
+      const data = await getLink(this.id)
+      this.url = data.data.data[0].url
+      this.buttonText = "狗叼，点击下载吧"
+      this.disabled = false
+    }
+  },
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="root">
+    <input v-model="id" placeholder="请输入音乐id"/>
+    <button @click="action">获取下载链接</button>
+    <a :href="url"><button :disabled="disabled">{{ buttonText }}</button></a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
